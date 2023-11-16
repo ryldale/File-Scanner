9dc635630058c6a8fb176b46c8ff8d545360dc31
ryldale <ryldale@risingtide.ph>
2023-11-16 15:41:53 +0800

    Initial Commit

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+// INCLUDE CONTENTS include(get_stylesheet_directory() . 'FILE_PATH');
+
+function my_theme_enqueue_styles() { 
+ wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+
+
+
+
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
